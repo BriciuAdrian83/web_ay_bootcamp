76eee4eec369ca5b896da2af6b9953ae877c3d52
BriciuAdrian83 <adrian.briciu83@gmail.com>
2024-11-03 20:29:46 +0200

    adding event listener to all buttons

--- javascript/drum_kit/index.js
+++ javascript/drum_kit/index.js
@@ -1,1 +1,9 @@
-alert('Script added');
+function handle_click() {
+    alert('I got clicked!');
+}
+
+const button_elements = document.querySelectorAll('.drum');
+const button_elements_length = document.querySelectorAll('.drum').length;
+for (let i = 0; i < button_elements_length; i++) {
+    button_elements[i].addEventListener('click', handle_click);
+}
