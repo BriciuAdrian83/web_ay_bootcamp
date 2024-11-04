d6b6f8e02463336879485917a7c52bd32f8ae2fc
BriciuAdrian83 <adrian.briciu83@gmail.com>
2024-11-04 17:58:35 +0200

    drum kit audio

--- javascript/drum_kit/index.js
+++ javascript/drum_kit/index.js
@@ -1,10 +1,21 @@
-function handle_click() {
-    var audio = new Audio('./sounds/tom-1.mp3');
+function play_audio() {
+    const obj_letter_to_song_files = {
+        'w': 'tom-1',
+        'a': 'tom-2',
+        's': 'tom-3',
+        'd': 'tom-4',
+        'j': 'snare',
+        'k': 'crash',
+        'l': 'kick-bass'
+    };
+
+    var audio = new Audio('./sounds/' + obj_letter_to_song_files[this.innerHTML] + '.mp3');
     audio.play();
+  
 }
 
 const button_elements = document.querySelectorAll('.drum');
 const button_elements_length = document.querySelectorAll('.drum').length;
 for (let i = 0; i < button_elements_length; i++) {
-    button_elements[i].addEventListener('click', handle_click);
+    button_elements[i].addEventListener('click', play_audio);
 }
