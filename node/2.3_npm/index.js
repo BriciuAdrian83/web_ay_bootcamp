e3e51acf3ba746282c1e1e07987b01cdcd5fc291
BriciuAdrian83 <adrian.briciu83@gmail.com>
2024-11-09 21:35:38 +0200

    practice using npm packages

---
+++ node/2.3_npm/index.js
@@ -0,0 +1,8 @@
+import generate_name from 'sillyname';
+import {randomSuperhero} from 'superheroes';
+
+var silly_name = generate_name();
+var superhero_random_name = randomSuperhero();
+
+
+console.log(`I'am ${superhero_random_name}!`);
