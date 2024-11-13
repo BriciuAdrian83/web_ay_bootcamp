73f4de0bd9bc48c4bb2f5f1663f5e2a3fcc0eb8a
BriciuAdrian83 <adrian.briciu83@gmail.com>
2024-11-13 08:57:42 +0200

    using server data in ejs if conditions

--- ejs/4.2_passing_date/index.js
+++ ejs/4.2_passing_date/index.js
@@ -1,14 +1,27 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render('index.ejs');
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const first_name = req.body['fName'];
+  const last_name = req.body['lName'];
+  const concatenated_fullname = first_name + last_name;
+  const nr_of_letters = concatenated_fullname.length;
+  res.render('index.ejs',
+    {
+      first_name: first_name,
+      last_name: last_name,
+      nr_of_letters: nr_of_letters,
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
