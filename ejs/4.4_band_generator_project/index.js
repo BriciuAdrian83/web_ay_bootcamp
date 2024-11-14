69f86c70f6bfc755948d5538b02bcb1e8964b82b
BriciuAdrian83 <adrian.briciu83@gmail.com>
2024-11-14 12:36:05 +0200

    band name generator adding css link to header and app.use(express.static('public')

--- ejs/4.4_band_generator_project/index.js
+++ ejs/4.4_band_generator_project/index.js
@@ -5685,6 +5685,7 @@ function generate_random_noun(arr_nouns) {
 //Hint: Google to find out how to get the current year using JS.
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
