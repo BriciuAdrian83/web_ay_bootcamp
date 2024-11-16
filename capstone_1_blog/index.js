743e0d98b2fb9b7d0a7cc9875b5152be2d5bd54d
BriciuAdrian83 <adrian.briciu83@gmail.com>
2024-11-16 15:47:01 +0200

    starting basic file structure realized render root dir with variables inserted in index.ejs partials included to index.ejs linking styles/main.css using for link delcaration of express static folder named public

--- capstone_1_blog/index.js
+++ capstone_1_blog/index.js
@@ -6,6 +6,15 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+
+app.get("/", (req, res) => {
+
+    res.render('index.ejs', {
+        title: 'All blog posts',
+        current_year: new Date().getFullYear(),
+    });
+});
+
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
 });
