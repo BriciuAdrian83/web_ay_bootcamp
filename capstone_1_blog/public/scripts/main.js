57b3d99bdc900985300c6ed7b1951328698e5caa
BriciuAdrian83 <adrian.briciu83@gmail.com>
2024-11-16 21:06:42 +0200

    adding main.js to the last commit

---
+++ capstone_1_blog/public/scripts/main.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+const bar1 = document.querySelector('.bar1');
+const bar2 = document.querySelector('.bar2');
+const bar3 = document.querySelector('.bar3');
+const mobile_nav = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', () => {
+    bar1.classList.toggle('animate_bar_1');
+    bar2.classList.toggle('animate_bar_2');
+    bar3.classList.toggle('animate_bar_3');
+    mobile_nav.classList.toggle('open-mobile-nav');
+})
