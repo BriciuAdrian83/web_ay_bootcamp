ebae373490335afeefa149dadd8896f691510a25
BriciuAdrian83 <adrian.briciu83@gmail.com>
2024-11-18 09:38:21 +0200

    capstone_1_blog basic create post submit and validation not empty

--- capstone_1_blog/index.js
+++ capstone_1_blog/index.js
@@ -6,22 +6,50 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+const posts = [];
 
 app.get("/", (req, res) => {
 
     res.render('index.ejs', {
         title: 'All blog posts',
-        current_year: new Date().getFullYear(),
+        currentYear: new Date().getFullYear(),
+        // posts,
     });
 });
 
 app.get("/create", (req, res) => {
     res.render("create.ejs", {
         title: "Create a Post",
-        current_year: new Date().getFullYear(),
+        currentYear: new Date().getFullYear(),
+        errors: {},
+        formData: { blogTitle: '', blogContent: ''},
     })
 });
 
+
+app.post("/save-post", (req, res) => {
+    const {"blog-title" : blogTitle, "blog-content": blogContent} = req.body;
+    const errors = {};
+
+    if (!blogTitle || blogTitle.trim() === "") {
+        errors.blogTitle = "Title is required.";
+    }
+    if (!blogContent || blogContent.trim() === "") {
+        errors.blogContent = "Content is required.";
+    }
+
+    if (Object.keys(errors). length > 0) {
+        return res.render("create.ejs", {
+            title: "Create a Post",
+            currentYear: new Date().getFullYear(),
+            errors,
+            formData: { blogTitle, blogContent }
+        });
+    }
+
+    posts.push({ title: blogTitle, content: blogContent });
+    res.redirect("/");
+});
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
