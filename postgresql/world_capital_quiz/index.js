cb008bc004381fd71d1b69beb3407b8907b3e6eb
BriciuAdrian83 <adrian.briciu83@gmail.com>
2024-12-14 17:17:42 +0200

    web_dev tried to get postgres working

--- postgresql/world_capital_quiz/index.js
+++ postgresql/world_capital_quiz/index.js
@@ -1,14 +1,38 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = 3000;
+
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: process.env.DB_POSTG_PASSWORD,
+  port: 5432
+});
+
+db.connect();
+
 
 let quiz = [
   { country: "France", capital: "Paris" },
   { country: "United Kingdom", capital: "London" },
   { country: "United States of America", capital: "New York" },
 ];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.log("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
